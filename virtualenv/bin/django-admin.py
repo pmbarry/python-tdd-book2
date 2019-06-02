dcb187ed51c6614a101336f5e840825593bac4e8
Paul Barry <paulbarry@Pauls-MacBook-Pro.local>
2019-06-02 19:36:43 +0100

    done

---
+++ virtualenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/paulbarry/Documents/DjangoProjects/python-tdd-book2/virtualenv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
